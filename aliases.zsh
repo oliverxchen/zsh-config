4e7ffd1e0f437397251e38ac95c952e52fc981c4
Oliver Chen <oliverxchen@gmail.com>
2026-01-19 12:38:52 +0800

    reminder on login_all command

--- aliases.zsh
+++ aliases.zsh
@@ -22,10 +22,14 @@ gc() {
   git fetch --all --prune
 }
 
+login_all() {
+  if ! (gcloud auth application-default login && gcloud auth login && firebase login --reauth); then
+    echo "If you get: \`ERROR: There was a problem with web authentication.\`, try to invoke the command \`gcloud auth revoke --all\` and run \`login_all\` again"
+  fi
+}
 
 alias docker="podman"
 alias docker_rm="podman ps -aq -f status=exited | xargs -r podman rm"
 alias docker_rmi="podman images -q -f 'dangling=true' | xargs -r podman rmi"
 alias docker_clean="docker_rm && docker_rmi"
 
-alias login_all="gcloud auth application-default login && gcloud auth login && firebase login --reauth"
